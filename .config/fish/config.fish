74075e5d002ae0720218d31005f93c5994f546ce
Christiaan Post <lcpost91@gmail.com>
2025-03-19 08:05:54 +0100

    new alias nvim .

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,13 @@
 set -x PATH $PATH /usr/local/go/bin ~/go/bin
 set -x TERM xterm-color
 set -x GO111MODULE on
-set -x AWS_REGION us-east-1
 set -x PATH $PATH /usr/local/texlive/2024/bin/x86_64-linux
+
+# android vars
+set -x CAPACITOR_ANDROID_STUDIO_PATH /snap/android-studio/186/bin/studio.sh
+set -x ANDROID_SDK_ROOT /home/ubuntu/Android/Sdk
+set -x ANDROID_HOME /home/ubuntu/Android/Sdk
+set -x ANDROID_SDK_ROOT /home/ubuntu/Android/Sdk
 
 # docker commands
 alias dup="docker compose up -d"
@@ -25,7 +30,7 @@ alias gt="git tag -l | sort -V | tail"
 alias yubi="ykman oath accounts code --single | wl-copy"
 
 # neovim shortcut
-alias n="nvim ."
+alias nn="nvim ."
 
 # add git command for dotfiles
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
