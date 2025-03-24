538e88afc09fd4fd8fe24693d52235e1a1ee68e2
Christiaan Post <lcpost91@gmail.com>
2025-03-24 15:07:13 +0100

    zellij layout changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,9 +5,8 @@ set -x PATH $PATH /usr/local/texlive/2024/bin/x86_64-linux
 
 # android vars
 set -x CAPACITOR_ANDROID_STUDIO_PATH /snap/android-studio/186/bin/studio.sh
-set -x ANDROID_SDK_ROOT /home/ubuntu/Android/Sdk
-set -x ANDROID_HOME /home/ubuntu/Android/Sdk
-set -x ANDROID_SDK_ROOT /home/ubuntu/Android/Sdk
+set -x ANDROID_HOME /home/Android/Sdk
+set -x ANDROID_SDK_ROOT /home/Android/Sdk
 
 # docker commands
 alias dup="docker compose up -d"
@@ -35,13 +34,17 @@ alias nn="nvim ."
 # add git command for dotfiles
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
+# lazygit for the dot files
 alias lazydot='lazygit --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+# zellij auto startup
+if status is-interactive
+    eval (zellij setup --generate-auto-start fish | string collect)
+end
+
 
 thefuck --alias | source
 
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 alias bat="/usr/local/bat/bat"
-
-# set JAVA_HOME to null, because it is set by lazy nvim
-set -x JAVA_HOME
---
+++ .config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,66 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = {
+        sign = true,
+        width = "block",
+        right_pad = 1,
+      },
+      heading = {
+        enabled = true,
+        sign = true,
+        position = "overlay",
+        icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+        signs = { "󰫎 " },
+        width = "full",
+        left_margin = 0,
+        left_pad = 0,
+        right_pad = 0,
+        min_width = 0,
+        border = false,
+        border_virtual = false,
+        border_prefix = false,
+        above = "▄",
+        below = "▀",
+        backgrounds = {
+          "RenderMarkdownH1Bg",
+          "RenderMarkdownH2Bg",
+          "RenderMarkdownH3Bg",
+          "RenderMarkdownH4Bg",
+          "RenderMarkdownH5Bg",
+          "RenderMarkdownH6Bg",
+        },
+        foregrounds = {
+          "RenderMarkdownH1",
+          "RenderMarkdownH2",
+          "RenderMarkdownH3",
+          "RenderMarkdownH4",
+          "RenderMarkdownH5",
+          "RenderMarkdownH6",
+        },
+      },
+      checkbox = {
+        enabled = true,
+      },
+    },
+    ft = { "markdown", "norg", "rmd", "org" },
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+      Snacks.toggle({
+        name = "Render Markdown",
+        get = function()
+          return require("render-markdown.state").enabled
+        end,
+        set = function(enabled)
+          local m = require("render-markdown")
+          if enabled then
+            m.enable()
+          else
+            m.disable()
+          end
+        end,
+      }):map("<leader>um")
+    end,
+  },
+}
